33c9b46ecb486e3e87e29779cb421fa3fed37b03
Shportaluk <andreyshportaluk@gmail.com>
2015-08-26 22:03:16 +0300

    26-08-2015 22:01
    Create repositories and add files

---
+++ 2 Thead/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _2_Thead
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[][] b = new byte[100][];
+
+            for (int i = 0; i < b.Length; i++)
+            {
+                b[i] = new byte[100];
+                for (int j = 0; j < b[i].Length; j++)
+                {
+                    b[i][j] = 1;
+                }
+            }
+
+            ThreadPool.QueueUserWorkItem( ShowSumCol, b );
+
+            Console.ReadKey();
+        }
+
+
+        static public void ShowSumCol(object obj)
+        {
+            byte[][] b = (byte[][])obj;
+
+            for (int i = 0; i < b.Length; i++)
+            {
+                ThreadPool.QueueUserWorkItem( SumCol, b[i] );
+                Thread.Sleep(100);
+            }
+        }
+
+
+        static public void SumCol(object obj)
+        {
+            byte[] b = (byte[])obj;
+
+            int sum = 0;
+
+            for (int i = 0; i < b.Length; i++)
+            {
+                sum += b[i];
+            }
+            Console.WriteLine("Sum: {0}", sum);
+        }
+    }
+}
---
+++ Thread/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Thread_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[][] b = new byte[100][];
+
+            for (int i = 0; i < b.Length; i++)
+            {
+                b[i] = new byte[100];
+                for ( int j = 0; j < b[i].Length; j++ )
+                {
+                    b[i][j] = 1;
+                }
+            }
+
+            Thread t = new Thread( ShowSumCol );
+            t.Start( b );
+            Console.ReadKey();
+        }
+
+
+        static public void ShowSumCol(object obj)
+        {
+            byte[][] b = (byte[][])obj;
+
+            for (int i = 0; i < b.Length; i++)
+            {
+                Thread c = new Thread(SumCol);
+                c.Start( b[i] );
+                Thread.Sleep(1);
+            }
+        }
+
+
+        static public void SumCol(object obj)
+        {
+            byte[] b = (byte[])obj;
+
+            int sum = 0;
+
+            for (int i = 0; i < b.Length; i++)
+            {
+                sum += b[i];
+            }
+            Console.WriteLine( "Sum: {0}", sum );
+        }
+
+    }
+}
